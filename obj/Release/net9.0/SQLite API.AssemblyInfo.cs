368e2f2bb7148bc455ea7cc65176fdb6cc850b4f
skysnowflok <jubileu1232020@gmail.com>
2025-02-21 15:21:34 -0300

    changed some stuff

--- obj/Release/net9.0/SQLite API.AssemblyInfo.cs
+++ obj/Release/net9.0/SQLite API.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("skysnowflok")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("A simple af SQLite API")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.1+861d34652f73cbd5cc19181f0ab47eacce154bd6")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.2.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.2+4edb4c5c65a13de571731158aff8d45b669c0253")]
 [assembly: System.Reflection.AssemblyProductAttribute("SQLite API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SQLite API")]
-[assembly: System.Reflection.AssemblyVersionAttribute("0.1.1.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.1.2.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
