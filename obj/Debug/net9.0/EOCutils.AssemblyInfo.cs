df6ed4ecfbb7b251f6dc337449cf28800ab47c6a
skysnowflok <jubileu1232020@gmail.com>
2025-02-24 05:23:24 -0300

    coisas

--- obj/Debug/net9.0/EOCutils.AssemblyInfo.cs
+++ obj/Debug/net9.0/EOCutils.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Uma coletania de ferramentas uteis para aplicativos de console.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.3.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.3+bc99252ba61f723b7d782d3b3ba3cff2b44680a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.3+cbbfe06841684f150ed2a39cf04c4e8ae6a9fefb")]
 [assembly: System.Reflection.AssemblyProductAttribute("EOCutils")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EOCutils")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.1.3.0")]
